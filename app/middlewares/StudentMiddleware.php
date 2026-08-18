4cb1105f51777bd9e955d2d92c9d5bef4f36b05e
Wreckkeyt21 <rickgayuso>
2026-08-18 17:00:44 +0800

    Laboratory Activity No.3

--- app/middlewares/StudentMiddleware.php
+++ app/middlewares/StudentMiddleware.php
@@ -4,14 +4,14 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 class StudentMiddleware
 {
     public function handle(Closure $next)
-    {
-        $required_code = 'GAYUSO-F1-2024';
+{
+    $required_code = 'GAYUSO-F1-2024';
 
-        if (!isset($_SESSION['rkc_access_code']) || $_SESSION['rkc_access_code'] !== $required_code) {
-            $_SESSION['access_message'] = '🔒 Access Restricted: Please click "Grant Access" first to view this profile.';
-            redirect('student');
-        }
-
-        return $next();
+    if (!isset($_SESSION['rkc_access_code']) || $_SESSION['rkc_access_code'] !== $required_code) {
+        $_SESSION['access_message'] = '🔒 Access Restricted: Please click "Login" first to view this profile.';
+        redirect('student');
     }
+
+    return $next();
+}
 }
