8100f1f1e0e5b895d9208fda96a34ef64873b8b3
Wreckkeyt21 <rickgayuso>
2026-09-09 22:43:56 +0800

    Add Lab 5: CRUD with Authentication and Aiven MySQL

--- app/config/database.php
+++ app/config/database.php
@@ -59,12 +59,12 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'   => 'mysql',
-    'hostname' => getenv('DB_HOST') ?: 'localhost',
-    'port'     => getenv('DB_PORT') ?: '3306',
-    'username' => getenv('DB_USERNAME') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: '',
-    'database' => getenv('DB_DATABASE') ?: 'mydb',
-    'charset'  => 'utf8mb4',
+    'hostname' => getenv('DB_HOST') ,
+    'port'     => getenv('DB_PORT') ,
+    'username' => getenv('DB_USERNAME') ,
+    'password' => getenv('DB_PASSWORD') ,
+    'database' => getenv('DB_DATABASE') ,
+    'charset'  => '',
     'dbprefix' => '',
     'path'     => ''
 );
